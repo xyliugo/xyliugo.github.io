e6d7b0c058570157d50978c02b11a83f89ea5acd
xyliugo <118035138+xyliugo@users.noreply.github.com>
2025-06-07 13:24:04 +0000

    Deploying to gh-pages from @ xyliugo/xyliugo.github.io@de6eb50b4fa5d63f6e0646a239fd24efbcda726c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -29,13 +29,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
-          },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown.html";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
